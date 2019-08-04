00cc72f319ccfcf6fa541e7397a58ea1d4ce4b94
gatakaba <m07112@gmail.com>
2019-08-04 21:24:01 +0900

    delete unnecessary process and add comment

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,16 +1,15 @@
 #! /bin/sh
-#ln -sf ~/dotfiles/.gitignore_global  ~/.gitignore_global
 
 for dot_file in $(ls -A); do
-    if [ $dot_file = ".git" ]; then
-        continue
-        echo hoge
-    fi
-    
+    # ignore running script 
     if [ $dot_file = $(basename $0) ]; then
         continue
     fi
-      
+    # ignore .git
+    if [ $dot_file = ".git" ]; then
+        continue
+    fi
+    # make link  
     echo $dot_file
     ln -sf ~/dotfiles/$dot_file ~/$dot_file
 done
