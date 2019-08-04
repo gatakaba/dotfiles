317551e03f4175baff75baea431953249716d725
gatakaba <m07112@gmail.com>
2019-08-04 21:13:25 +0900

    apply formatter

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,11 +1,9 @@
 #! /bin/sh
 #ln -sf ~/dotfiles/.gitignore_global  ~/.gitignore_global
 
-for dot_file in `ls -A`;
-do
-        if [ $dot_file = `basename $0` ];
-        then
-                continue
-        fi
-        echo $dot_file
+for dot_file in $(ls -A); do
+	if [ $dot_file = $(basename $0) ]; then
+		continue
+	fi
+	echo $dot_file
 done
