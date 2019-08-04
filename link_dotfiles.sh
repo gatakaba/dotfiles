cb4038a123517b37101eca389972195e78ab65b0
gatakaba <m07112@gmail.com>
2019-08-04 21:21:40 +0900

    delete readme and ignore .git

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -2,8 +2,15 @@
 #ln -sf ~/dotfiles/.gitignore_global  ~/.gitignore_global
 
 for dot_file in $(ls -A); do
+    if [ $dot_file = ".git" ]; then
+        continue
+        echo hoge
+    fi
+    
     if [ $dot_file = $(basename $0) ]; then
         continue
     fi
+      
     echo $dot_file
+    ln -sf ~/dotfiles/$dot_file ~/$dot_file
 done
