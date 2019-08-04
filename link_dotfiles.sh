f26fda6e19bd2042e7ed428e74a0c43a9b246cde
gatakaba <m07112@gmail.com>
2019-08-04 21:01:07 +0900

    initial comit

---
+++ link_dotfiles.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+#ln -sf ~/dotfiles/.gitignore_global  ~/.gitignore_global
+
+for dot_file in `ls -A`;
+do
+        if [ $dot_file = `basename $0` ];
+        then
+                continue
+        fi
+        echo $dot_file
+done
