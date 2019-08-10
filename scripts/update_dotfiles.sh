ea333256ca5d9d5fbe43fbc8e3f5c4b03d60a7a2
gatakaba <m07112@gmail.com>
2019-08-10 18:25:15 +0900

    fix link script

--- scripts/update_dotfiles.sh
+++ scripts/update_dotfiles.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
-
-for dot_file in ../.??*; do
+cd ../
+for dot_file in .??*; do
     # ignore .git
     if [ $dot_file = ".git" ]; then
         continue
     fi
     # make link  
     echo $dot_file
-    ln -sf ~/dotfiles/$dot_file ~/$dot_file
+    ln -sf -v  ~/dotfiles/$dot_file ~/$dot_file
 done
