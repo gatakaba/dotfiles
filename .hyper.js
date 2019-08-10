a9be268e21dac57b8fdbe70bc9129cd65f3a0f72
gatakaba <m07112@gmail.com>
2019-08-10 18:36:35 +0900

    add hyper-search

--- .hyper.js
+++ .hyper.js
@@ -146,7 +146,14 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyperlinks','hypercwd','hyper-tab-icons','hyper-pane','verminal'],
+  plugins: [
+    'hyperlinks',
+    'hypercwd',
+    'hyper-tab-icons',
+    'hyper-pane',
+    'verminal',
+    "hyper-search"
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
