b2fa7576606d5cb2a32c2397e5a5fd7fcdea4929
gatakaba <m07112@gmail.com>
2019-08-15 21:14:11 +0900

    fix src path

--- install.sh
+++ install.sh
@@ -1,62 +1,17 @@
 #! /bin/sh
 
 function setup_vscode() {
-    # link vs code config file(mac only)
+    # link vscode config file(mac only)
     if [ $(uname) = "Darwin" ]; then
-        ln -sf -v ~/dotfiles/vscode.json "$HOME/Library/Application Support/Code/User/settings.json"
+        ln -sf -v "$PWD/configs/vscode/vscode.json" "$HOME/Library/Application Support/Code/User/settings.json"
     else
         echo "Set VS Code config file manually"
     fi
 
     # install extention
-    for extention in $(cat ./configs/vscode/vscode-extention-list); do
+    for extention in $(cat $PWD/configs/vscode/vscode-extention-list); do
         code --install-extension $extention
     done
 }
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-9,0-1 全て
+
+setup_vscode
